76b01c2e3fa9498e04a71e41668f658b72d1910b
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-03-06 22:01:23 +0530

    [add] api for get api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import UserSchema from './users.schema';
 
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -18,4 +19,9 @@ export class UsersService {
   async findByEmail(email: string) {
     return await this.userModel.findOne({ email }).exec();
   }
+
+  async findById(id) {
+    return await this.userModel.findById(id).exec();
+  }
+
 }
